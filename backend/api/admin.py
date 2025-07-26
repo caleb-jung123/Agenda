00193f0dd6803112582ef118a0b823ae55e0efeb
justintran40 <justintran40@gmail.com>
2025-07-25 23:51:20 -0400

    Basic Tag Implementation
    Implemented tag and added serializers.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
